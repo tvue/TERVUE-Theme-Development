473e1b2182d24af96243835db3f27194637ce329
Tuater Vue <tvue@cals.wisc.edu>
2015-03-31 22:16:51 -0500

    Added parallax effect to header

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -116,5 +116,95 @@ jQuery(document).ready(function($) {
   */
   loadGravatars();
 
+         function parallaxIt(obj,ratio,imgtype,following,header) {
+           if(!header) {
+             header = false;
+           }
+
+           $(window).scroll(function() {
+             var offset = obj.offset().top;
+             var objH = obj.height();
+             var wH = $(window).height();
+             var newtop = $(window).scrollTop();
+
+             if(imgtype == "background") {
+               if(header) {
+                 newtophalf = ""+(50 - ((newtop-offset) * ratio)) +"%";
+                 headertranslate = "50% "+newtophalf+"";
+               } else {
+                 newtophalf = ""+((newtop-offset) * ratio) +"px";
+                 headertranslate = "0px "+newtophalf+"";
+               }
+
+               if(offset<(newtop+wH) && (offset+objH)>(newtop-objH)) {
+                 //console.log("in view");
+                 $(obj).css("background-position",headertranslate);
+                 $(following).css("background-position",headertranslate);
+               }
+             } else {
+                 if(offset<(newtop+wH) && (offset+objH)>(newtop-objH)) {
+
+
+                   if(header) {
+
+                     
+                     if($(obj).next().attr("id") == "cholder") {
+                     } else {
+                       newtophalf = ""+(newtop / (1+ratio)) +"px";
+                       newtop = "-"+newtop+"px";
+                       newtranslate = "translate3d(0px,"+newtop+",0px)";
+                       headertranslate = "translate3d(0px,"+newtophalf+",0px)";
+
+                       $(obj).css("transform",headertranslate);
+                       $(following).css("transform",headertranslate);
+                     }
+
+
+                   } else {
+                     $(obj).css("transform",headertranslate);
+                     $(following).css("transform",headertranslate);
+                   }
+                 }
+
+
+             }
+
+           });
+       }
+
+
+
+
+       function inView(obj,threshholdBottom,threshholdTop) {
+           var offset = $(".header " + obj).offset().top;
+           var objH = $(".header " + obj).height();
+
+
+           function calcView() {
+             var wH = $(window).height();
+             var newtop = $(window).scrollTop();
+
+                //console.log("object: "+offset + " " + (newtop+wH) + "   maxobj: " + (offset+objH)+" "+(newtop-objH));
+               if(((offset+threshholdBottom)<(newtop+wH)) && ((offset-threshholdTop)>(newtop-objH))) {
+
+                 $(obj).addClass("inView");
+
+               } else {
+
+                 $(obj).removeClass("inView");
+               }
+           }
+
+           $(window).scroll(function() {
+             calcView();
+
+           });
+
+           calcView();
+       }
+
+
+       parallaxIt($(".site-branding"),0.13,'background','',true);
+
 
 }); /* end of as page load scripts */
