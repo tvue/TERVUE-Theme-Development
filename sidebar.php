a78656b4c0d65532fe188cacc1d0aaa9ba616727
Tuater Vue <tvue@cals.wisc.edu>
2015-03-31 22:17:42 -0500

    created a menu svg icon for header on page

--- sidebar.php
+++ sidebar.php
@@ -19,3 +19,12 @@
 					<?php endif; ?>
 
 				</div>
+
+				<?php//show sidebar if active
+
+				if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+						return;}
+					?>
+				<div id="secondary" class="widget-area" role="complementary">
+					<?php dynamic_sidebar( 'sidebar-1' ); ?>
+				</div><!-- #secondary -->
